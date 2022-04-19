2dcdbfb42636c942255d93bff48a138111d61186
“Natalie <natalie.liles@mavenclinic.com>
2022-04-18 20:35:33 -0400

    update map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import usMapData from './us';
 import { geoAlbersUsa, geoPath } from 'd3-geo'
 import Tree from './Tree'
-import geoAlbersUsaTerritories from "geo-albers-usa-territories";
+import { geoAlbersUsaTerritories } from "d3-composite-projections";
+import * as topojson from "https://cdn.skypack.dev/topojson@3.0.2";
 import styles from './Map.module.css'
 
 export const Map = ({ data = [], seletedData = []}) => {
-    // const newData = topojson.feature(statesInfo, statesInfo.objects.collection).features
+
+    const usData = topojson.feature(usMapData, usMapData.objects.states)
 
     //Width and height of maps
     const width = 960;
-    const height = 500;
+    const height = 530;
 
-    const projection = geoAlbersUsa().translate([width/2, height/2]).scale(1000);          // scale things down so see entire US
+    const projection = geoAlbersUsaTerritories().translate([width/2, height/2]).scale(1000);          // scale things down so see entire US
     const pathGenerator = geoPath().projection(projection);
 
     const parks = data.map((d, i) => {
       const cords = projection([d.longitude, d.latitude])
-      const y = cords?.[0]
       const x = cords?.[1]
+      const y = cords?.[0]
       return (
         <image key={d.id} x={y} y={x} width="50" height="50" href="./tree.png">
         </image>
        )
     })
 
-    const states = usMapData.features.map((d) => <path
+    const states = usData.features.map((d) => <path
        key={d.properties?.name}
        d={pathGenerator(d)}
        className={styles.state}
@@ -33,13 +35,12 @@ export const Map = ({ data = [], seletedData = []}) => {
      )
 
     return (
-      <>
-      <Tree />
-      <svg width={960} height={500}>
-        {states}
-        {parks}
-      </svg>
-      </>
+      <div className={styles.mapContainer}>
+        <svg width={width} height={height}>
+          {states}
+          {parks}
+        </svg>
+      </div>
     )
 
 }
