daad0de87cb41c4d3602b11d469c2fa98603cdef
Natalie <natalie.liles@mavenclinic.com>
2022-09-10 12:01:56 -0400

    update alt text

--- src/components/Map/Tooltip/index.tsx
+++ src/components/Map/Tooltip/index.tsx
@@ -22,15 +22,12 @@ const Tooltip = ({ park, coords }: TooltipType) => {
   };
 
   const imageSrc = imageErr ? "np.svg" : image?.url || "np.svg";
-  const altTxt = imageErr
-    ? "No image displayed"
-    : image?.altText || "No image displayed";
 
   return (
     <foreignObject className={styles.tooltip} x={x} y={y}>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
-          <img src={imageSrc} alt={altTxt} onError={handleImgError} />
+          <img src={imageSrc} alt={image?.altText} onError={handleImgError} />
         </div>
         <div className={styles.text}>
           <h1>{park.fullName}</h1>
