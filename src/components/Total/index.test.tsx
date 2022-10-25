36c98931c0efeca45a18155d9abd2e801435724e
Natalie <natalie.liles@mavenclinic.com>
2022-10-24 17:46:00 -0700

    add tests

---
+++ src/components/Total/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Total from '.';
+
+describe('<Total />', () => {
+  it('Displays the correct count', () => {
+    render(<Total count={10} total={20} />);
+    expect(screen.getByText('10')).toBeVisible();
+    expect(screen.getByText('20')).toBeVisible();
+  });
+});
---
+++ src/components/ui/Checkbox/index.test.tsx
@@ -0,0 +1,47 @@
+import { act, cleanup, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Formik, Form } from "formik";
+import Checkbox from '.';
+
+const mockOnChange = jest.fn();
+
+const getForm = () => (
+  <Formik enableReinitialize onSubmit={() => {}} initialValues={{}}>
+    {() => (
+      <Form>
+        <Checkbox id="1" label="Park" name="Park" handleChange={mockOnChange}  />
+      </Form>
+    )}
+  </Formik>
+)
+
+describe('<Checkbox />', () => {
+
+  afterEach(cleanup);
+
+  it('Displays the correct label', () => {
+    render(
+      getForm()
+    );
+    expect(screen.getByLabelText('Park')).toBeVisible();
+  });
+
+  it('Call mockOnChange onClick', () => {
+    render(
+      getForm()
+    );
+    userEvent.click(screen.getByLabelText('Park'))
+    expect(mockOnChange).toHaveBeenCalledWith(['1']);
+  });
+
+  fit('Toggles checkbox', () => {
+    render(
+      getForm()
+    );
+    const checkbox = screen.getByLabelText('Park') as HTMLInputElement;
+    userEvent.click(checkbox)
+    expect(checkbox.checked).toBe(true)
+    userEvent.click(checkbox)
+    expect(checkbox.checked).toBe(false)
+  });
+});
