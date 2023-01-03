52b09ae61e9d929f7499c05dea3e4e6cb44912d4
Natalie <natalie.liles@mavenclinic.com>
2023-01-02 20:21:10 -0500

    update test

--- src/components/ui/Input/index.test.tsx
+++ src/components/ui/Input/index.test.tsx
@@ -1,17 +1,18 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { Formik, Form } from "formik";
+import { useForm, FormProvider } from "react-hook-form";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-const getForm = () => (
-  <Formik enableReinitialize onSubmit={() => {}} initialValues={{}}>
-    {() => (
-      <Form>
+const getForm = () => {
+  const methods = useForm({ defaultValues: {} });
+  return (
+    <FormProvider {...methods}>
+      <form>
         <Input id="1" label="Email" formError="Error" />
-      </Form>
-    )}
-  </Formik>
-);
+      </form>
+    </FormProvider>
+  );
+};
 
 describe("<Input />", () => {
   it("Displays correct label and placeholder", () => {
--- src/screens/Stats/StatsPage/Count/index.tsx
+++ src/screens/Stats/StatsPage/Count/index.tsx
@@ -1,14 +1,11 @@
+import { CountProps } from './types'
 import styles from "./index.module.scss";
 
 const Count = ({
   header,
   count,
   total,
-}: {
-  header: string;
-  count: number;
-  total: number;
-}) => {
+}: CountProps) => {
   return (
     <div className={styles.container}>
       <h3 className={styles.header}>{header}</h3>
