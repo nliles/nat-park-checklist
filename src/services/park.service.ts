5ca12823b94b1c5a03a0317080afb5b4f6714e7b
Natalie <natalie.liles@mavenclinic.com>
2023-01-12 16:40:44 -0500

    revert header change

--- src/services/park.service.ts
+++ src/services/park.service.ts
@@ -2,11 +2,25 @@ import { apiClient } from "services/apiService";
 import { ParkDesignationType } from "enum/ParkDesignation";
 import { Parks } from "types";
 
-export const getParks = (): Promise<{ parks: Parks }> => apiClient.get("/park");
+export const getParks = (): Promise<{ parks: Parks }> => {
+  return apiClient.get("/park", {
+  headers: {
+    crossdomain: true,
+    Authorization: sessionStorage.getItem("token") || "",
+    "Content-Type": "application/json",
+  },
+});
+}
 
 export const updateParks = (
   designation: ParkDesignationType,
   parks: string[]
 ): Promise<{ parks: Parks }> => {
-  return apiClient.post("/park", { designation, parks });
+  return apiClient.post("/park", { designation, parks }, {
+    headers: {
+      crossdomain: true,
+      Authorization: sessionStorage.getItem("token") || "",
+      "Content-Type": "application/json",
+    },
+  });
 };
