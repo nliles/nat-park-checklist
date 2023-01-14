501290908cc6135584c4425a018999b6875e767d
Natalie <natalie.liles@mavenclinic.com>
2023-01-14 10:34:17 -0500

    update use effect dependency

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -13,7 +13,7 @@ const AuthProvider = ({ children }: { children: ReactNode }) => {
       sessionStorage.removeItem("token");
       dispatch(logoutSuccess());
     }
-  }, [token, isExpired]);
+  }, [token, isExpired, dispatch]);
 
   return <>{children}</>;
 };
