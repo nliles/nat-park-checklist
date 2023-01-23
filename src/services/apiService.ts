ae7104850526182fe29efa5d790cc4eb134dc9b1
Natalie <natalie.liles@mavenclinic.com>
2023-01-23 09:01:28 -0500

    update prod vs dev url

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 import { logoutSuccess } from "actions";
 import toast from "react-hot-toast";
 import store from 'store';
-import { baseURL } from './constants';
+import { devBaseURL, prodBaseURL, DEV_ENV } from './constants';
 
 axios.defaults.withCredentials = true
 axios.defaults.headers.common.crossdomain = true;
@@ -10,7 +10,7 @@ axios.defaults.headers.common["Content-Type"] = "application/json";
 
 // Main API base
 export const apiClient = axios.create({
-  baseURL,
+  baseURL: DEV_ENV ? devBaseURL : prodBaseURL,
 });
 
 const handleError = (error: AxiosError) => {
--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,1 +1,5 @@
-export const baseURL = "https://nat-park-checklist.herokuapp.com/";
+export const prodBaseURL = "https://nat-park-checklist.herokuapp.com/";
+
+export const devBaseURL = 'http://localhost:5000/';
+
+export const DEV_ENV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
