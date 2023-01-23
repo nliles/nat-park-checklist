f6aefa273340ee9b90b5aff7ad0621555f5b7c2d
Natalie <natalie.liles@mavenclinic.com>
2023-01-23 11:12:44 -0500

    update toast duration

--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -6,7 +6,11 @@ import { State } from "reducers/types";
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const isLoggedIn = useSelector((state: State) => !!state.auth.user);
   if (!isLoggedIn) {
-    <Navigate to="/" replace />;
+    return (
+      <>
+        <Navigate to="/" replace />;
+      </>
+    )
   } else {
     return <>{children}</>;
   }
