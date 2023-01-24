de0f1f7f5f085fbdcaae37eab1742d1ffd3f1650
Natalie <natalie.liles@mavenclinic.com>
2023-01-23 22:36:51 -0500

    delete file

--- src/components/Tooltip/types.ts
+++
@@ -1,9 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-import { Park } from "types/park";
-
-export type TooltipProps = {
-  coords: number[];
-  park: Park;
-  tooltipId: string;
-  setTooltipContent: Dispatch<SetStateAction<Park | undefined>>;
-};
