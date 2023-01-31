aafd490e4974b24b37ec7b695ca01d7f12236a1f
Natalie <natalie.liles@mavenclinic.com>
2023-01-31 15:23:14 -0500

    update types

--- src/types/park.ts
+++ src/types/park.ts
@@ -1,16 +1,5 @@
 type Address = {
-  city: string;
-  line1: string;
-  line2: string;
-  line3: string;
-  postalCode: string;
   stateCode: string;
-  type: string;
-};
-
-type ParkActivities = {
-  id: string;
-  name: string;
 };
 
 type Image = {
@@ -18,28 +7,14 @@ type Image = {
   altText: string;
 };
 
-export type ParkLatLong = {
-  id: string;
-  latitude: string;
-  longitude: string;
-};
-
 export type Park = {
   addresses?: Address[];
-  activities?: ParkActivities[];
-  description?: string;
-  designation?: string;
-  directionsInfo?: string;
-  directionsUrl?: string;
   fullName?: string;
   id: string;
   images?: Image[];
   latitude: string;
   longitude: string;
-  latLong?: string;
   name?: string;
-  parkCode?: string;
   states?: string;
   url?: string;
-  weather?: string;
 };
