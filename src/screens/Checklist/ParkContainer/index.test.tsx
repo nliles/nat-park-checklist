36240782e5606c7216f397a432c673b238837158
Natalie <natalie.liles@mavenclinic.com>
2023-01-31 13:47:54 -0500

    add park container tests

--- src/screens/Checklist/ParkContainer/index.test.tsx
+++ src/screens/Checklist/ParkContainer/index.test.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
-import { render, screen, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
 import ParkContainer from 'screens/Checklist/ParkContainer';
 
 jest.mock("react-redux", () => ({
@@ -11,13 +11,26 @@ jest.mock("react-redux", () => ({
 describe('<ParkContainer/>', () => {
 	it('Displays the correct default designation', () => {
 		render(
-			<BrowserRouter>
+			<MemoryRouter initialEntries={[{ pathname: '/' }]}>
 			  <ParkContainer />
-			</BrowserRouter>
+			</MemoryRouter>
 	);
 		expect(
 			screen.getByText(
 				'National Parks'
+			)
+		).toBeVisible();
+	});
+
+	it('Displays the correct designation from route', () => {
+		render(
+			<MemoryRouter initialEntries={[{ pathname: '/', search: '?designation=national-battlefield-park' }]}>
+				<ParkContainer />
+			</MemoryRouter>
+	);
+		expect(
+			screen.getByText(
+				'National Battlefield Parks'
 			)
 		).toBeVisible();
 	});
