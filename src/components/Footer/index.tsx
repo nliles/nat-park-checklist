afbc6ac49347231d550e2e95e026d1f9d6fcb19f
Natalie Liles <natalie.liles@mavenclinic.com>
2024-02-08 09:19:54 -0500

    add aria label to footer icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,9 +9,9 @@ const Footer = () => {
       <div className={styles.links}>
         {icons.map((i) => (
           <a href={i.link} key={i.link}>
-            {i.icon === Icon.LINKED_IN && <FaLinkedin />}
-            {i.icon === Icon.GITHUB && <FaGithub />}
-            {i.icon === Icon.INSTAGRAM && <FaInstagram />}
+            {i.icon === Icon.LINKED_IN && <FaLinkedin aria-label="LinkedIn"/>}
+            {i.icon === Icon.GITHUB && <FaGithub aria-label="Github"/>}
+            {i.icon === Icon.INSTAGRAM && <FaInstagram aria-label="Instagram"/>}
           </a>
         ))}
       </div>
