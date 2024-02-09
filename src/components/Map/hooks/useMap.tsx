b7c723f701d2670add772093ceb16bd4b409ef81
Natalie Liles <natalie.liles@mavenclinic.com>
2024-02-09 13:57:21 -0500

    adjust scale

--- src/components/Map/hooks/useMap.tsx
+++ src/components/Map/hooks/useMap.tsx
@@ -79,7 +79,7 @@ function useMap(
           dy = bounds[1][1] - bounds[0][1],
           x = (bounds[0][0] + bounds[1][0]) / 2,
           y = (bounds[0][1] + bounds[1][1]) / 2,
-          scale = .7 / Math.max(dx / width, dy / height),
+          scale = .5 / Math.max(dx / width, dy / height),
           translate = [width / 2 - scale * x, height / 2 - scale * y];
       
         d3.select("#map g").transition()
