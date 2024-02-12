61fe86e0bbc249904c85541ed8962228d1dc2057
Natalie Liles <natalie.liles@mavenclinic.com>
2024-02-12 17:38:05 -0500

    add state hover

--- src/components/Map/hooks/useMap.tsx
+++ src/components/Map/hooks/useMap.tsx
@@ -161,7 +161,14 @@ function useMap(
         }
       }
 
-      d.on("click", function (event, d) {
+      d
+      .on("mouseover", function (event, d) {
+         d3.select(this).style("fill", "#d8d2bc")
+      })
+      .on("mouseout", function (event, d) {
+        d3.select(this).style("fill", active.node() === this ? "#d8d2bc" : "#eae3d1")
+     })
+      .on("click", function (event, d) {
         active.classed(styles.active, false);
         if (active.node() === this) return reset();
         active = d3.select(this).classed(styles.active, true);
