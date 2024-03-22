626d16b84c667428a25d33ee3dcb8cbec29a0da7
Natalie Liles <natalie.liles@mavenclinic.com>
2024-03-22 18:07:13 -0400

    apply prettier

--- src/helpers/getParkDesignation.ts
+++ src/helpers/getParkDesignation.ts
@@ -3,18 +3,18 @@ import camelCase from "lodash/camelCase";
 import ParkDesignation from "enum/ParkDesignation";
 
 const getParkDesignation = (designation: string, parkCode: string) => {
-    const formattedDesignation = camelCase(designation);
-    if (LIST_OPTIONS.includes(formattedDesignation as ParkDesignation)) {
-      return formattedDesignation;
-    } 
-    return Object.values(ParkDesignation).find((parkKey) => {
-      if (PARK_INFO[parkKey]) {
-        if (PARK_INFO[parkKey].codes.includes(parkCode)) {
-          return parkKey;
-        }
+  const formattedDesignation = camelCase(designation);
+  if (LIST_OPTIONS.includes(formattedDesignation as ParkDesignation)) {
+    return formattedDesignation;
+  }
+  return Object.values(ParkDesignation).find((parkKey) => {
+    if (PARK_INFO[parkKey]) {
+      if (PARK_INFO[parkKey].codes.includes(parkCode)) {
+        return parkKey;
       }
-      return ParkDesignation.OTHER_DESIGNATION;
-    });
-  };
+    }
+    return ParkDesignation.OTHER_DESIGNATION;
+  });
+};
 
-  export default getParkDesignation;
+export default getParkDesignation;
