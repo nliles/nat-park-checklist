9ad125d432ed15abc30e6f3f04d74b4ec75492b9
Natalie Liles <natalie.liles@mavenclinic.com>
2024-03-31 16:51:31 -0400

    remove unused component

--- src/components/ui/CircularProgressBar/CircularProgressBar.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import CircularProgressBar from ".";
-
-describe("<CircularProgressBar />", () => {
-  it("Displays the correct percentage", () => {
-    render(<CircularProgressBar percentage={50} />);
-    expect(screen.getByText("50%")).toBeVisible();
-  });
-});
