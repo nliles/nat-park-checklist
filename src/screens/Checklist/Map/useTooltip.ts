c8e39ac0c77d8152db3296743765855fb987e0eb
Natalie Liles <natalie.liles@mavenclinic.com>
2024-04-04 16:47:34 -0400

    update map folder structure

--- src/screens/Checklist/Map/useTooltip.ts
+++ src/screens/Checklist/Map/useTooltip.ts
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import * as d3 from "d3";
 import cn from "classnames";
 import { hideTooltip } from "screens/Checklist/Map/handleTooltip";
-import styles from "../Map.module.scss";
+import styles from "./Map.module.scss";
 
 function useTooltip() {
   const handleKeyDown = (e: KeyboardEvent) => {
