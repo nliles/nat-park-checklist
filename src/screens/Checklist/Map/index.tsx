24a9c44975a34bd7086f77f99ed0f56c32e9a1c3
Natalie Liles <natalie.liles@mavenclinic.com>
2024-04-09 08:54:55 -0400

    remove id

--- src/screens/Checklist/Map/index.tsx
+++ src/screens/Checklist/Map/index.tsx
@@ -238,7 +238,7 @@ const Map = ({ parks = [] }: MapProps) => {
     drawMap();
   });
 
-  return <div ref={containerRef} className={styles.mapContainer} id="mapContainer"/>;
+  return <div ref={containerRef} className={styles.mapContainer}/>;
 };
 
 export default Map;
