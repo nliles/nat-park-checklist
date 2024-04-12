9d94c3fc3e0586b11a16c5311589e53eb5e6feb0
Natalie Liles <natalie.liles@mavenclinic.com>
2024-04-12 08:56:01 -0400

    apply prettier

--- src/screens/Checklist/Map/index.tsx
+++ src/screens/Checklist/Map/index.tsx
@@ -5,26 +5,26 @@ import useTooltip from "screens/Checklist/Map/hooks/useTooltip";
 import useMap from "screens/Checklist/Map/hooks/useMap";
 import styles from "./Map.module.scss";
 
-const Map = ({ parks = [] }: { parks: Park[]}) => {
+const Map = ({ parks = [] }: { parks: Park[] }) => {
   const { containerRef, width, height } = useContainerWidth();
   useTooltip();
-  useMap({ width, height, parks })
-  
+  useMap({ width, height, parks });
+
   return (
     <div ref={containerRef} className={styles.mapContainer} id="mapContainer">
-    <div className={styles.buttons}>
-      <button className={styles.home} id="home">
-        <FaHome aria-label="Instagram" />
-      </button>
-      <div className={styles.plusMinus}>
-       <button className={styles.plus} id="plus">
-        <FaPlus aria-label="Instagram" />
-      </button>
-      <button className={styles.minus} id="minus">
-        <FaMinus aria-label="Instagram" />
-      </button> 
+      <div className={styles.buttons}>
+        <button className={styles.home} id="home">
+          <FaHome aria-label="Instagram" />
+        </button>
+        <div className={styles.plusMinus}>
+          <button className={styles.plus} id="plus">
+            <FaPlus aria-label="Instagram" />
+          </button>
+          <button className={styles.minus} id="minus">
+            <FaMinus aria-label="Instagram" />
+          </button>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
