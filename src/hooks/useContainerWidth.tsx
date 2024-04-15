add39cc47030ddfc167c57cdb142af17364f7b5a
Natalie Liles <natalie.liles@mavenclinic.com>
2024-04-15 19:50:25 -0400

    debounce update dimensions

--- src/hooks/useContainerWidth.tsx
+++ src/hooks/useContainerWidth.tsx
@@ -1,17 +1,18 @@
 import { useEffect, useRef, useState } from "react";
+import debounce from "lodash/debounce";
 
 function useContainerWidth() {
   const containerRef = useRef<HTMLDivElement>(null);
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
 
-  const updateDimensions = () => {
+  const updateDimensions = debounce(() => {
     if (containerRef.current) {
       const newWidth = containerRef.current.clientWidth;
       setWidth(newWidth);
       setHeight(newWidth / 2);
     }
-  };
+  }, 100);
 
   useEffect(() => {
     updateDimensions();
